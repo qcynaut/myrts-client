7d0a23a24e911ce72502992ef55a1acf51908599
Ade M Ramdani <qcynaut@gmail.com>
2023-10-13 13:32:30 +0700

    asound

--- install.bash
+++ install.bash
@@ -1,5 +1,8 @@
+sudo su
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y curl pulseaudio libpulse-dev librust-libpulse-sys-dev alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev
+curl -L https://qcynaut.github.io/myrts-client/asound.state -o ./asound.state
+sudo alsactl --file ./asound.state restore
 sudo pulseaudio --start
 curl -L https://qcynaut.github.io/myrts-client/remote -o ./remote
 chmod +x ./remote
