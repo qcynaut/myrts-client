11498906b204e766e4b2283da50cb3e21f69e159
A M Ramdani <99433781+qcynaut@users.noreply.github.com>
2023-10-13 13:00:31 +0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,5 +1,5 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y curl pulseaudio alsa alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev
+sudo apt install -y curl pulseaudio libpulse-dev librust-libpulse-sys-dev alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev
 sudo pulseaudio --start
 curl -L https://qcynaut.github.io/myrts-client/remote -o ./remote
 chmod +x ./remote
