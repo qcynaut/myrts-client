3445d9062c43a6c978ab443b023eef83e9ebaccb
Ade M Ramdani <qcynaut@gmail.com>
2023-10-13 14:24:34 +0700

    asound

--- install.bash
+++ install.bash
@@ -5,6 +5,11 @@ rm /etc/systemd/system/myrts-remote.service
 mkdir /var/lib/myrts
 echo -n "Nama device:"
 read device
+while [ -z "$device" ]
+do
+    echo -n "Nama device:"
+    read device
+done
 echo $device > /var/lib/myrts/desc.txt
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y curl pulseaudio libpulse-dev librust-libpulse-sys-dev alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev
@@ -14,3 +19,4 @@ sudo pulseaudio --start
 curl -L https://qcynaut.github.io/myrts-client/remote -o ./remote
 chmod +x ./remote
 sudo ./remote
+#make other code after `read device` wait for user to fill the `read`
