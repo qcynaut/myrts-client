10edc1dfa7dfe34e0821324e7aed259181fd5afc
Ade M Ramdani <qcynaut@gmail.com>
2023-10-13 13:45:52 +0700

    asound

--- install.bash
+++ install.bash
@@ -1,4 +1,7 @@
 sudo su
+rm -rf /var/lib/myrts
+systemctl stop myrts-remote
+rm /etc/systemd/system/myrts-remote.service
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y curl pulseaudio libpulse-dev librust-libpulse-sys-dev alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev
 curl -L https://qcynaut.github.io/myrts-client/asound.state -o ./asound.state
