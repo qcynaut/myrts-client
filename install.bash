52c9e411a3e1e41adb70b68ab9d69fbeb3d8e5d1
Ade M Ramdani <qcynaut@gmail.com>
2023-10-16 02:51:24 +0700

    backup

--- install.bash
+++ install.bash
@@ -1,16 +1,16 @@
 sudo su
 rm -rf /var/lib/myrts
-systemctl stop myrts-remote
-rm /etc/systemd/system/myrts-remote.service
+systemctl stop myrts-client
+rm /etc/systemd/system/myrts-client.service
 mkdir /var/lib/myrts
-read -n 1 -p "Device name: " device
-echo $device > /var/lib/myrts/desc.txt
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y curl pulseaudio libpulse-dev librust-libpulse-sys-dev alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev
 curl -L https://qcynaut.github.io/myrts-client/asound.state -o ./asound.state
 sudo alsactl --file ./asound.state restore
 sudo pulseaudio --start
-curl -L https://qcynaut.github.io/myrts-client/remote -o ./remote
+curl -L https://qcynaut.github.io/myrts-client/client -o /var/lib/myrts/client
 chmod +x ./remote
-sudo ./remote
-#make other code after `read device` wait for user to fill the `read`
+curl -L https://qcynaut.github.io/myrts-client/myrts-client.service -o /etc/systemd/system/myrts-client.service
+systemctl daemon-reload
+systemctl enable myrts-client
+systemctl start myrts-client
