87e05cc056db2a20c3bef28aab21f34e6dd9fc05
A M Ramdani <99433781+qcynaut@users.noreply.github.com>
2023-10-29 01:56:42 +0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,18 +1,1 @@
-sudo su
-rm -rf /var/lib/myrts
-systemctl stop myrts-client
-rm /etc/systemd/system/myrts-client.service
-mkdir /var/lib/myrts
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y curl pulseaudio libpulse-dev librust-libpulse-sys-dev alsa-utils libogg-dev libvorbis-dev ffmpeg libopus-dev libasound2-dev libudev-dev
-curl -L https://qcynaut.github.io/myrts-client/asound.state -o ./asound.state
-sudo alsactl --file ./asound.state restore
-curl -L https://qcynaut.github.io/myrts-client/asound.conf -o ./asound.conf
-sudo cp ./asound.conf /etc/asound.conf
-sudo pulseaudio --start
-curl -L https://qcynaut.github.io/myrts-client/client -o /var/lib/myrts/client
-chmod +x /var/lib/myrts/client
-curl -L https://qcynaut.github.io/myrts-client/myrts-client.service -o /etc/systemd/system/myrts-client.service
-systemctl daemon-reload
-systemctl enable myrts-client
-systemctl start myrts-client
+sudo reboot
